e0b9b504783a28a87330392a767bc8e15bd2f01e
Antonio05550 <joanrivasna@ittepic.edu.mx>
2020-06-13 13:54:22 -0600

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,230 @@
+from __future__ import print_function
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from scipy import stats
+from IPython.display import display, HTML
+
+from sklearn import metrics
+from sklearn.metrics import classification_report
+from sklearn import preprocessing
+
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Reshape
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+
+# Set some standard parameters upfront
+from tensorflow_core.python.keras.utils import np_utils
+
+pd.options.display.float_format = '{:.1f}'.format
+# Default seaborn look and feel
+sns.set()
+plt.style.use('ggplot')
+print('keras version ', keras.__version__)
+
+# Same labels will be reused throughout the program
+LABELS = ['Caminar',
+          'Descansar',
+          'Trotar']
+
+# The number of steps within one time segment
+TIME_PERIODS = 80
+
+# The steps to take from one segment to the next; if this value is equal to
+# TIME_PERIODS, then there is no overlap between the segments
+STEP_DISTANCE = 40
+
+
+def read_data(file_path):
+    column_names = ['user-id',
+                    'activity',
+                    'timestamp',
+                    'x-axis',
+                    'y-axis',
+                    'z-axis']
+    df = pd.read_csv(file_path,
+                     header=None,
+                     names=column_names)
+    # Last column has a ";" character which must be removed and then this column must be transformed to float explicitly
+    df['z-axis'].replace(regex=True,
+                         inplace=True,
+                         to_replace=r';',
+                         value=r'')
+    df['z-axis'] = df['z-axis'].apply(convert_to_float)
+
+    # This is very important otherwise the model will not fit and loss will show up as NAN
+    df.dropna(axis=0, how='any', inplace=True)
+
+    return df
+
+
+def convert_to_float(x):
+    try:
+        return np.float(x)
+    except:
+        return np.nan
+
+
+def show_basic_dataframe_info(dataframe):
+    # Shape and how many rows and columns
+    print('Number of columns in the dataframe: %i' % (dataframe.shape[1]))
+    print('Number of rows in the dataframe: %i\n' % (dataframe.shape[0]))
+
+
+# Load data set containing all the data from csv
+df = read_data('DB.txt')
+show_basic_dataframe_info(df)
+df.head(5)
+
+# Show how many training examples exist for each of the six activities
+df['activity'].value_counts().plot(kind='bar', title='Training Examples by Activity Type')
+plt.show()
+
+# Better understand how the recordings are spread across the different users who participated in the study
+df['user-id'].value_counts().plot(kind='bar', title='Training Examples by User')
+plt.show()
+
+# Define column name of the label vector
+LABEL = 'ActivityEncoded'
+
+# Transform the labels from String to Integer via LabelEncoder
+le = preprocessing.LabelEncoder()
+
+# Add a new column to the existing DataFrame with the encoded values
+df[LABEL] = le.fit_transform(df['activity'].values.ravel())
+
+# Differentiate between test set and training set
+df_test = df[df['user-id'] > 28]
+df_train = df[df['user-id'] <= 28]
+
+
+# Normalize features for training data set (values between 0 and 1)
+# Surpress warning for next 3 operation
+pd.options.mode.chained_assignment = None  # default='warn'
+df_train['x-axis'] = df_train['x-axis'] / df_train['x-axis'].max()
+df_train['y-axis'] = df_train['y-axis'] / df_train['y-axis'].max()
+df_train['z-axis'] = df_train['z-axis'] / df_train['z-axis'].max()
+# Round numbers
+df_train = df_train.round({'x-axis': 4, 'y-axis': 4, 'z-axis': 4})
+
+
+def create_segments_and_labels(df, time_steps, step, label_name):
+    # x, y, z acceleration as features
+    N_FEATURES = 3
+
+    # Number of steps to advance in each iteration (for me, it should always
+    # be equal to the time_steps in order to have no overlap between segments)
+    # step = time_steps
+    segments = []
+    labels = []
+    for i in range(0, len(df) - time_steps, step):
+        xs = df['x-axis'].values[i: i + time_steps]
+        ys = df['y-axis'].values[i: i + time_steps]
+        zs = df['z-axis'].values[i: i + time_steps]
+        # Retrieve the most often used label in this segment
+        label = stats.mode(df[label_name][i: i + time_steps])[0][0]
+        segments.append([xs, ys, zs])
+        labels.append(label)
+
+    # Bring the segments into a better shape
+    reshaped_segments = np.asarray(segments, dtype=np.float32).reshape(-1, time_steps, N_FEATURES)
+    labels = np.asarray(labels)
+
+    return reshaped_segments, labels
+
+
+x_train, y_train = create_segments_and_labels(df_train,
+                                              TIME_PERIODS,
+                                              STEP_DISTANCE,
+                                              LABEL)
+
+print('x_train shape: ', x_train.shape)
+print('y_train shape: ', y_train.shape)
+
+# Set input & output dimensions
+num_time_periods, num_sensors = x_train.shape[1], x_train.shape[2]
+num_classes = le.classes_.size
+print(list(le.classes_))
+
+input_shape = (num_time_periods * num_sensors)
+x_train = x_train.reshape(x_train.shape[0], input_shape)
+print('x_train shape:', x_train.shape)
+print('input_shape:', input_shape)
+
+x_train = x_train.astype('float32')
+y_train = y_train.astype('float32')
+
+y_train_hot = np_utils.to_categorical(y_train, num_classes)
+print('New y_train shape: ', y_train_hot.shape)
+
+model_m = Sequential()
+# Remark: since coreml cannot accept vector shapes of complex shape like
+# [80,3] this workaround is used in order to reshape the vector internally
+# prior feeding it into the network
+model_m.add(Reshape((TIME_PERIODS, 3), input_shape=(input_shape,)))
+model_m.add(Dense(100, activation='relu'))
+model_m.add(Dense(100, activation='relu'))
+model_m.add(Dense(100, activation='relu'))
+model_m.add(Flatten())
+model_m.add(Dense(num_classes, activation='softmax'))
+print(model_m.summary())
+
+model_m.compile(loss='categorical_crossentropy',
+                optimizer='adam', metrics=['accuracy'])
+
+# Hyper-parameters
+BATCH_SIZE = 400
+EPOCHS = 50
+
+# Enable validation to use ModelCheckpoint and EarlyStopping callbacks.
+history = model_m.fit(x_train,
+                      y_train_hot,
+                      batch_size=BATCH_SIZE,
+                      epochs=EPOCHS,
+                      validation_split=0.2,
+                      verbose=1)
+
+
+
+# Convert the model.
+converter = tf.lite.TFLiteConverter.from_keras_model(model_m)
+tflite_model = converter.convert()
+
+# Save the TF Lite model.
+with tf.io.gfile.GFile('model.tflite', 'wb') as f:
+  f.write(tflite_model)
+
+# Create a converter
+converter = tf.lite.TFLiteConverter.from_keras_model(model_m)
+# Convert the model
+tflite_model = converter.convert()
+# Create the tflite model file
+tflite_model_name = "mymodel.tflite"
+open(tflite_model_name, "wb").write(tflite_model)
+
+# Convert Keras model to TF Lite format.
+converter = tf.lite.TFLiteConverter.from_keras_model(model_m)
+tflite_float_model = converter.convert()
+
+# Show model size in KBs.
+float_model_size = len(tflite_float_model) / 1024
+print('Float model size = %dKBs.' % float_model_size)
+
+# Re-convert the model to TF Lite using quantization.
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_quantized_model = converter.convert()
+
+# Show model size in KBs.
+quantized_model_size = len(tflite_quantized_model) / 1024
+print('Quantized model size = %dKBs,' % quantized_model_size)
+print('which is about %d%% of the float model size.'\
+      % (quantized_model_size * 100 / float_model_size))
+
+# Save the quantized model to file to the Downloads directory
+f = open('recoact.tflite', "wb")
+f.write(tflite_quantized_model)
+f.close()
